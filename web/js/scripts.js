1ca0b3cba295c8e3212b7dca5fbe77aaa2b037f0
Yurii Trofimenko <tyaa@ukr.net>
2017-07-24 20:01:34 +0300

    add base files

---
+++ web/js/scripts.js
@@ -0,0 +1,11 @@
+$.getScript("js/hogan/hogan-3.0.2.min.js", function(){
+
+   console.log("hogan loaded");
+
+});
+
+$.getScript("js/bootstrap/bootstrap.min.js", function(){
+
+   console.log("bootstrap loaded");
+
+});
